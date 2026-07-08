0f09d1b73ba91dacea74ffdfb2740c9eff713415
Veronica Molina <veronica04molina@gmail.com>
2026-07-08 18:25:33 -0500

    Creación del proyecto Spring Boot BarberApp

---
+++ src/main/java/com/barberapp/barberapp/BarberappApplication.java
@@ -0,0 +1,13 @@
+package com.barberapp.barberapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberappApplication.class, args);
+	}
+
+}
